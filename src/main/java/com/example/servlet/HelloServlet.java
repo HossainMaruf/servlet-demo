ac2918f4afcfec6c1760b622354e6460733119df
HossainMaruf <maruficepustian@gmail.com>
2025-11-13 14:22:30 +0600

    JSP Added

--- src/main/java/com/example/servlet/HelloServlet.java
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -7,20 +7,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        // resp.getWriter().println("<title>Document</title>");
-        // resp.getWriter().println("<h1>Maruf Hossain 2</h1>");
-        // resp.getWriter().println("<script>alert('Hey')</script>");
-        resp.getWriter().println("""
-            <html>
-                <head>
-                    <title>Document Title </title>
-                </head>
-                <body>
-                    <h1>Md. Maruf Hossain</h1>
-                </body>
-                </head>
-            </html>
-        """);
+        req.setAttribute("message", "Hello From SERVLET");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Servlet Demo</title>
+</head>
+<body>
+    <h1>Hello from JSP!</h1>
+    <p>Current time: <%= new java.util.Date() %></p>
+</body>
+</html>
