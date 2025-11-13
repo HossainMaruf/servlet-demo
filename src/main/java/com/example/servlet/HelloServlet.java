4fffb39743717834924fb382eaf1a8d6b2f22b00
HossainMaruf <maruficepustian@gmail.com>
2025-11-13 12:58:25 +0600

    Updated build plugins to java 21

--- src/main/java/com/example/servlet/HelloServlet.java
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -8,6 +8,19 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().println("<h1>Maruf Hossain 2</h1>");
+        // resp.getWriter().println("<title>Document</title>");
+        // resp.getWriter().println("<h1>Maruf Hossain 2</h1>");
+        // resp.getWriter().println("<script>alert('Hey')</script>");
+        resp.getWriter().println("""
+            <html>
+                <head>
+                    <title>Document Title </title>
+                </head>
+                <body>
+                    <h1>Md. Maruf Hossain</h1>
+                </body>
+                </head>
+            </html>
+        """);
     }
 }
