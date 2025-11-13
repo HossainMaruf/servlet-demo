650820339b88b391bc78d1c387a7baa97b6bf1d8
HossainMaruf <maruficepustian@gmail.com>
2025-11-13 12:18:35 +0600

    Initial

---
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Maruf Hossain 2</h1>");
+    }
+}
